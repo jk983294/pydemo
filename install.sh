6cee361df1ab89c8430233643a1910aaacb73d66
jk983294 <jk983294@gmail.com>
2022-07-06 16:54:15 +0800

    build cmd

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ python3 -m pip install --upgrade build
 cd python
 python3 -m build --wheel
 python3 -m pip install python/dist/pydemo_package_kun-0.0.1-py3-none-any.whl
+
+python setup.py bdist_wheel
